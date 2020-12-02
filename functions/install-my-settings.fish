cbd1b512525e790e40c8021c72d11ee931f724d7
Tomohiro Nishimura <tomohiro68@gmail.com>
2020-12-02 23:09:19 +0900

    fix typo

--- functions/install-my-settings.fish
+++ functions/install-my-settings.fish
@@ -3,7 +3,7 @@ function install-my-settings
 
   set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
   set -l fish_config_home $XDG_CONFIG_HOME/fish
-  set -l config_path $fish_config_home/functions/configh.fish
+  set -l config_path $fish_config_home/functions/config.fish
 
   cd $fish_config_home
   ln -s $config_path .
