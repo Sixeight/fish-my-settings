e98b105ab948777f2139214320d35a31a35d87ad
Tomohiro Nishimura <tomohiro68@gmail.com>
2021-02-07 15:16:48 +0900

    delete plenv prompt

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -1,8 +1,5 @@
 function fish_right_prompt
   set -l ruby_version (rbenv version-name)
-  # set -l perl_version (plenv version-name)
   set -l node_version (nodenv version-name)
-
-  # echo -n "$ruby_version / $perl_version / $node_version |" (date +%H:%M)
   echo -n "$ruby_version / $node_version |" (date +%H:%M)
 end
